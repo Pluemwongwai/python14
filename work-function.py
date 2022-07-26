ea51560017cbe6e0f93c249af6b9a6099ba2781a
Pluemwongwai <50239@cru.ac.th>
2022-07-26 14:27:10 +0700

    commit

---
+++ work-function.py
@@ -0,0 +1,16 @@
+#คำสั่งหาพื้นที่สี่เหลี่ยม
+def squre(width,height):
+    ans = width*height
+    print(ans)
+squre(5,5)
+#คำสั่งหาพื้นที่สามเหลี่ยม
+def triangle(base,height):
+    ans = 1/2*base*height
+    print(ans)
+triangle(10,5)
+#คำสั่งหาพื้นที่วงกลม
+def circle(radius):
+    ans = 22/7*radius*radius
+    print(ans)
+circle(7)
+#นายณรงค์ฤทธิ์ ว่องไว เลขที่ 34 ชั้น ม6/14
